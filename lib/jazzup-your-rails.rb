9da7b7e8d3b86daa66a5fa1d04105e1552de94f7
Dominik Menke <github@dmke.de>
2013-11-20 00:06:50 +0100

    typofix

--- lib/jazzup-your-rails.rb
+++ lib/jazzup-your-rails.rb
@@ -4,7 +4,7 @@ class JazzupYourRails
 
     if `which afplay` != ""
       pid  = fork{ exec 'afplay', file }
-    else `which mpg123` != ""
+    elsif `which mpg123` != ""
       pid = fork{ exec 'mpg123','-q', file }
     end
   end
